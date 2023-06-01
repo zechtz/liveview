a262d1041ebad4c91618f9b66223e44e7a32d4fc
Zachariah Ngonyani <zachariahn@gmail.com>
2023-06-01 17:17:49 +0300

    feat: finish light component

--- lib/live_view_app_web/live/light_live.ex
+++ lib/live_view_app_web/live/light_live.ex
@@ -2,11 +2,14 @@ defmodule LiveViewAppWeb.LightLive do
   use LiveViewAppWeb, :live_view
 
   def mount(_params, _sesssion, socket) do
+    # IO.inspect(self(), label: "MOUNT")
     socket = assign(socket, brightness: 10)
     {:ok, socket}
   end
 
   def render(assigns) do
+    # IO.inspect(self(), label: "RENDER")
+
     ~H"""
     <h1>Front Porch Light</h1>
     <div id="light">
@@ -15,10 +18,16 @@ defmodule LiveViewAppWeb.LightLive do
           <%= @brightness %>
         </span>
       </div>
-      <button>
+      <button phx-click="off">
         <img src="/images/light-off.svg" />
       </button>
-      <button>
+      <button phx-click="down">
+        <img src="/images/down.svg" />
+      </button>
+      <button phx-click="up">
+        <img src="/images/up.svg" />
+      </button>
+      <button phx-click="on">
         <img src="/images/light-on.svg" />
       </button>
     </div>
@@ -26,4 +35,27 @@ defmodule LiveViewAppWeb.LightLive do
   end
 
   # handle_event
+  def handle_event("on", _payload, socket) do
+    # IO.inspect(self(), label: "ON")
+    socket = assign(socket, brightness: 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _payload, socket) do
+    # IO.inspect(self(), label: "OFF")
+    socket = assign(socket, brightness: 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _payload, socket) do
+    # brightness = socket.assigns.brightness + 10
+    socket = update(socket, :brightness, &(&1 + 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _payload, socket) do
+    # brightness = socket.assigns.brightness - 10
+    socket = update(socket, :brightness, &(&1 - 10))
+    {:noreply, socket}
+  end
 end
