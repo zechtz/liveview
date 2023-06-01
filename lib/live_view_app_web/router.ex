e06ccc273563bee4e4263e95f490a50eae59e749
Zachariah Ngonyani <zachariahn@gmail.com>
2023-06-01 16:20:05 +0300

    feat: define light live route

--- lib/live_view_app_web/router.ex
+++ lib/live_view_app_web/router.ex
@@ -18,6 +18,8 @@ defmodule LiveViewAppWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    live "/light", LightLive
   end
 
   # Other scopes may use custom stacks.
