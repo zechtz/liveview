1ff4eae79e40eb6d74053c52f445b0fd82da0934
Zachariah Ngonyani <zachariahn@gmail.com>
2023-06-01 19:42:02 +0300

    feat: add sandbox route

--- lib/live_view_app_web/router.ex
+++ lib/live_view_app_web/router.ex
@@ -20,6 +20,7 @@ defmodule LiveViewAppWeb.Router do
     get "/", PageController, :home
 
     live "/light", LightLive
+    live "/sandbox", SandboxLive
   end
 
   # Other scopes may use custom stacks.
